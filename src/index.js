421f5af2f129a90bc70022f45d800dff89708046
61114440694 <sakkarin.ha.61@ubu.ac.th>
2023-08-08 21:52:49 +0700

    React-Mid-Restaurant

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@700&display=swap" rel="stylesheet" />
     <App />
   </React.StrictMode>
 );
